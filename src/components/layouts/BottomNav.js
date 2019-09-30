140ced5f3c1347cce553355b95de8827fe049590
Azmol-Miah <azmol@azmolmiah.com>
2019-09-30 19:49:14 +0100

    fixed nav to bottom

--- src/components/layouts/BottomNav.js
+++ src/components/layouts/BottomNav.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
@@ -10,13 +10,15 @@ import EventIcon from '@material-ui/icons/Event';
 
 const useStyles = makeStyles({
   root: {
-    maxWidth: '100%'
+    width: '100%',
+    position: 'fixed',
+    bottom: 0
   }
 });
 
 export default function LabelBottomNavigation() {
   const classes = useStyles();
-  const [value, setValue] = React.useState('recents');
+  const [value, setValue] = useState('recents');
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
