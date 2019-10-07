918dbbf0ab0c75ce1b157dacac7dd432421731d1
Azmol-Miah <azmol@azmolmiah.com>
2019-10-07 09:32:40 +0100

    destructured icons import

--- src/components/layouts/BottomNav.js
+++ src/components/layouts/BottomNav.js
@@ -2,11 +2,18 @@ import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import HomeIcon from '@material-ui/icons/Home';
-import MenuBookIcon from '@material-ui/icons/MenuBook';
-import LocalMallIcon from '@material-ui/icons/LocalMall';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import EventIcon from '@material-ui/icons/Event';
+import {
+  Home,
+  MenuBook,
+  LocalMall,
+  AccountCircle,
+  Event
+} from '@material-ui/icons';
+// import HomeIcon from '@material-ui/icons/Home';
+// import MenuBookIcon from '@material-ui/icons/MenuBook';
+// import LocalMallIcon from '@material-ui/icons/LocalMall';
+// import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+// import EventIcon from '@material-ui/icons/Event';
 
 const useStyles = makeStyles({
   root: {
@@ -34,27 +41,27 @@ export default function LabelBottomNavigation() {
       <BottomNavigationAction
         label='Home'
         value='home'
-        icon={<HomeIcon style={{ color: '#fff' }} />}
+        icon={<Home style={{ color: '#fff' }} />}
       />
       <BottomNavigationAction
         label='Bookings'
         value='bookings'
-        icon={<EventIcon style={{ color: '#fff' }} />}
+        icon={<Event style={{ color: '#fff' }} />}
       />
       <BottomNavigationAction
         label='Menu'
         value='menu'
-        icon={<MenuBookIcon style={{ color: '#fff' }} />}
+        icon={<MenuBook style={{ color: '#fff' }} />}
       />
       <BottomNavigationAction
         label='Cart'
         value='cart'
-        icon={<LocalMallIcon style={{ color: '#fff' }} />}
+        icon={<LocalMall style={{ color: '#fff' }} />}
       />
       <BottomNavigationAction
         label='Account'
         value='account'
-        icon={<AccountCircleIcon style={{ color: '#fff' }} />}
+        icon={<AccountCircle style={{ color: '#fff' }} />}
       />
     </BottomNavigation>
   );
