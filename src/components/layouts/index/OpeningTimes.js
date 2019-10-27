dce2016055e4588b77397257348f8896e2fdcecf
Azmol-Miah <azmol@azmolmiah.com>
2019-10-27 10:40:06 +0000

    made table and time into objects in state and then mapped through them

--- src/components/layouts/index/OpeningTimes.js
+++ src/components/layouts/index/OpeningTimes.js
@@ -1,47 +1,36 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
-
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 
+// Make table day and times into objects then map through them in the table boy
+
 const OpeningTimes = () => {
+  const state = [
+    { day: 'Sun', time: 'Closed' },
+    { day: 'Mon', time: '05:00pm - 11:00pm' },
+    { day: 'Tue', time: '05:00pm - 11:00pm' },
+    { day: 'Wed', time: '05:00pm - 11:00pm' },
+    { day: 'Thu', time: '10:00am - 10:00pm' },
+    { day: 'Fri', time: '01:00pm - 04:00am' },
+    { day: 'Sat', time: '05:00pm - 11:00pm' }
+  ];
+
   return (
     <div>
       <h1>Opening Times</h1>
       Times may change on public holidays
       <Table>
         <TableBody>
-          <TableRow>
-            <TableCell>Sun</TableCell>
-            <TableCell>Closed</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Mon</TableCell>
-            <TableCell>05:00pm - 11:00pm</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Tue</TableCell>
-            <TableCell>05:00pm - 11:00pm</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Wed</TableCell>
-            <TableCell>05:00pm - 11:00pm</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Thu</TableCell>
-            <TableCell>10:00am - 10:00pm</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Fri</TableCell>
-            <TableCell>01:00pm - 04:00am</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Sat</TableCell>
-            <TableCell>05:00pm - 11:00pm</TableCell>
-          </TableRow>
+          {state.map(row => {
+            return (
+              <TableRow key={row.day}>
+                <TableCell>{row.day}</TableCell>
+                <TableCell>{row.time}</TableCell>
+              </TableRow>
+            );
+          })}
         </TableBody>
       </Table>
     </div>
