8c5f567f10510ed4f09a8e54f1ee786b9d5c776c
Azmol-Miah <azmol@azmolmiah.com>
2019-11-03 20:03:51 +0000

    completed the footer, just need the links

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -3,6 +3,8 @@ import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
+import Box from '@material-ui/core/Box';
+import RefreshIcon from '@material-ui/icons/Refresh';
 
 const Footer = () => {
   const styles = {
@@ -21,7 +23,7 @@ const Footer = () => {
               age old recipes from Indian sub continent.
             </p>
           </Grid>
-          <Grid item xs={12} md={3}>
+          <Grid item xs={6} md={3}>
             <h2>Customer Service</h2>
             <List>
               <li>Login</li>
@@ -29,7 +31,7 @@ const Footer = () => {
               <li>Help Chat</li>
             </List>
           </Grid>
-          <Grid item xs={12} md={3}>
+          <Grid item xs={6} md={3}>
             <h2>Information</h2>
             <List>
               <li>Terms of Use</li>
@@ -42,24 +44,47 @@ const Footer = () => {
         </Grid>
         <Divider light />
         <Grid container justify='center'>
-          <img
-            src='https://waiterplus.com/webappcdn/2.6.94/assets/img/payment-wallet.png'
-            alt='wallet'
-          />
-          <img
-            src='https://waiterplus.com/webappcdn/2.6.94/assets/img/payment-mastercard.png'
-            alt='mastercard'
-          />
-          <img
-            src='https://waiterplus.com/webappcdn/2.6.94/assets/img/payment-visa.png'
-            alt='visa'
-          />
-          <img
-            src='https://waiterplus.com/webappcdn/2.6.94/assets/img/payment-paypal.png'
-            alt='paypal'
-          />
+          <Box m={1}>
+            <img
+              style={{ height: '30px' }}
+              src='https://waiterplus.com/webappcdn/2.6.94/assets/img/payment-wallet.png'
+              alt='wallet'
+            />
+          </Box>
+          <Box m={1}>
+            <img
+              style={{ height: '30px' }}
+              src='https://waiterplus.com/webappcdn/2.6.94/assets/img/payment-mastercard.png'
+              alt='mastercard'
+            />
+          </Box>
+          <Box m={1}>
+            <img
+              style={{ height: '30px' }}
+              src='https://waiterplus.com/webappcdn/2.6.94/assets/img/payment-visa.png'
+              alt='visa'
+            />
+          </Box>
+          <Box m={1}>
+            <img
+              style={{ height: '30px' }}
+              src='https://waiterplus.com/webappcdn/2.6.94/assets/img/payment-paypal.png'
+              alt='paypal'
+            />
+          </Box>
         </Grid>
         <Divider light />
+
+        <Grid container justify='center' xs={12}>
+          <Box pt={1}>
+            &copy; {new Date().getFullYear()} Powered by: WaiterPlus 2.6.94
+          </Box>
+        </Grid>
+        <Grid container justify='center' xs={12}>
+          <Box mb={8}>
+            <RefreshIcon /> Reset App
+          </Box>
+        </Grid>
       </Container>
     </div>
   );
